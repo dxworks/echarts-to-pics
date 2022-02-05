18a952610362d410bd22291db10619947dfc90a8
ioana-lazar <62338641+ioana-lazar@users.noreply.github.com>
2022-02-05 20:40:57 +0200

    optional commandline arg for picture size

--- src/functions.js
+++ src/functions.js
@@ -7,13 +7,5 @@ export function print(args) {
 export function usage(){
     console.log(chalk.dim("\nGenerate graphs images by parsing a folder containing their JSON config files.\n"));
     console.log(chalk.red(" Use:"));
-    console.log("       generate-images <folder-name>\n")
+    console.log("       generate-images <folder-name> *<image-size>\n")
 }
-
-export function sleep(milliseconds) {
-    const date = Date.now();
-    let currentDate = null;
-    do {
-      currentDate = Date.now();
-    } while (currentDate - date < milliseconds);
-  }
--- src/generate-images.js
+++ src/generate-images.js
@@ -1,93 +1,121 @@
 #!/usr/bin/env node
 
-import { print, sleep, usage } from "./functions.js";
+import { print, usage } from "./functions.js";
 import fs from 'fs';
 import chalk from 'chalk';
 import * as echarts from 'echarts';
 import { JSDOM } from 'jsdom';
 import ora from "ora";
+import sharp from "sharp";
 
 //just a lil animation in the terminal
 const spinner = ora('working');
 print('\n');
 spinner.start();
-//sleep(500);
+
+//variables
+var counter = 0;
+var image_size = 1000;
 
 //check command-line args
 const args = process.argv;
-if (args.length != 3){
+if ((args.length < 3) || (args.length > 4)){
     spinner.stop();
     usage();
     process.exit(1);
 }
+else{
+    if(args.length == 4)
+        image_size = process.argv[3];
 
-//check given folder of JSONs
-var folder_path = process.argv[2];
-fs.readdir(folder_path, function(err, items){
-    if (err) {
-        spinner.fail(chalk.red("Error on folder path\n"));
-        process.exit(1);
-      }
-
-    if(!items){
-        spinner.fail(chalk.red("Error on folder path\n"));
-        process.exit(1);
-    }
-
-    //create new directory for generated charts
-    const new_folder_name = '../GeneratedImages'
-    if (!fs.existsSync(new_folder_name)) {
-        fs.mkdirSync(new_folder_name)
-      }
-
-        
-    //read each folder entry
-    for (var i=0; i<items.length; i++) {
-        if(items[i].substring(items[i].length - 5, items[i].length) != '.json'){
-            spinner.fail(chalk.red("Error encountered on a file type\n"));
+    
+    //check given folder of JSONs
+    var folder_path = process.argv[2];
+    fs.readdir(folder_path, function(err, items){
+        if (err) {
+            spinner.fail(chalk.red(`Could not find folder path ${folder_path}\n`));
             process.exit(1);
         }
 
-        var filepath = folder_path + '/' + items[i];
-        var data = fs.readFileSync(filepath);
-
-        //config object
-        var option;
-        try{ 
-            option = JSON.parse(data);
-          } catch(e) { 
-            spinner.fail(chalk.red("Error on JSON file format\n"));
+        if(!items){
+            spinner.fail(chalk.red("Error on folder path\n"));
             process.exit(1);
-          }
+        }
 
-        //generate chart
-        echarts.setPlatformAPI(() => {
-            return createCanvas(100, 100);
-        });
- 
-        const {window} = new JSDOM();
-        global.window = window;
-        global.navigator = window.navigator;
-        global.document = window.document;
-        
-        const root = document.createElement('div');
-        root.style.cssText = 'width: 500px; height: 500px;';
-        
-        Object.defineProperty(root, "clientWidth", {value: 1000});
-        Object.defineProperty(root, "clientHeight", {value: 1000});
-        
-        const chart = echarts.init(root, null, {
-            renderer: 'svg'
-        });
+        //create new directory for generated charts
+        const new_folder_name = '../GeneratedImages'
+        if (!fs.existsSync(new_folder_name)) {
+            fs.mkdirSync(new_folder_name)
+        }
 
-        chart.setOption(option);
+            
+        //read each folder entry
+        for (var i=0; i<items.length; i++) {
+            if(items[i].substring(items[i].length - 5, items[i].length) != '.json'){
+                spinner.warn(chalk.yellow(`Wrong file type at ${items[i]}\n`));
+                continue;
+            }
 
-        //save as svg
-        var new_file = '../GeneratedImages/'+ items[i].substring(0, items[i].length - 4) + 'svg';
-        fs.writeFileSync(new_file, root.querySelector('svg').outerHTML, 'utf-8');
+            var filepath = folder_path + '/' + items[i];
+            var data = fs.readFileSync(filepath);
 
-        chart.dispose();
-    }    
+            //config object
+            var option;
+            try{ 
+                option = JSON.parse(data);
+            } catch(e) { 
+                option = "error";
+            }
 
-    spinner.succeed(chalk.green(" finished\n"));
-});
+            if(option == "error"){
+                spinner.warn(chalk.yellow(`Wrong JSON format on file ${items[i]}\n`));
+                continue;
+            }
+            
+            //generate chart
+            echarts.setPlatformAPI(() => {
+                return createCanvas(100, 100);
+            });
+
+            const {window} = new JSDOM();
+            global.window = window;
+            global.navigator = window.navigator;
+            global.document = window.document;
+            
+            const root = document.createElement('div');
+            root.style.cssText = 'width: 500px; height: 500px;';
+            
+
+            var value; //prompt user for 
+            Object.defineProperty(root, "clientWidth", {value: image_size});
+            Object.defineProperty(root, "clientHeight", {value: image_size});
+            
+            const chart = echarts.init(root, null, {
+                renderer: 'svg'
+            });
+
+            chart.setOption(option);
+
+            //save as svg
+            var new_file = new_folder_name + '/' + items[i].substring(0, items[i].length - 4) + 'svg';
+            fs.writeFileSync(new_file, root.querySelector('svg').outerHTML, 'utf-8');
+
+            //convert to png - doesn't work yet 
+            //console.log(new_file)
+            /*sharp(new_file)
+                .png()
+                .toFile(`${new_file.substring(0, new_file.length - 3) + 'png'}`)
+                .then(function(info) {
+                    console.log(info)
+                })
+                .catch(function(err) {
+                    console.log(err)
+                })*/
+
+            chart.dispose();  
+            counter++;        
+        }   
+        spinner.succeed(chalk.green(" finished\n"));
+        print(chalk.dim(`Generated ${counter} charts\n`));
+    });   
+}
